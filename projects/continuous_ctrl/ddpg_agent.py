0ad86d06f80213835ca24feba42dbe1a606cb512
John Stark <starkj@leidos.com>
2021-02-11 06:19:13 -0800

    WIP. Avg score ~0.4

--- projects/continuous_ctrl/ddpg_agent.py
+++ projects/continuous_ctrl/ddpg_agent.py
@@ -10,19 +10,19 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e4)  # replay buffer size
-BATCH_SIZE = 128        # minibatch size
-GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 1e-3        # learning rate of the critic
-WEIGHT_DECAY = 0        # L2 weight decay
+#BATCH_SIZE = 32         # minibatch size; orig 128
+GAMMA = 0.99            # discount factor; orig 0.99
+TAU = 0.001             # for soft update of target parameters; orig 0.001
+LR_ACTOR = 0.0001       # learning rate of the actor; orig 0.0001
+LR_CRITIC = 0.001       # learning rate of the critic; orig 0.001
+WEIGHT_DECAY = 0.01        # L2 weight decay; orig 0
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class DdpgAgent():
     """Interacts with and learns from the environment."""
     
-    def __init__(self, state_size, action_size, random_seed):
+    def __init__(self, state_size, action_size, random_seed, batch_size=32):
         """Initialize an Agent object.
         
         Params
@@ -34,22 +34,26 @@ class DdpgAgent():
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(random_seed)
+        self.batch_size = batch_size
+
+        layer1_units = 128
+        layer2_units = 48
 
         # Actor Network (w/ Target Network)
-        self.actor_local = Actor(state_size, action_size, random_seed).to(device)
-        self.actor_target = Actor(state_size, action_size, random_seed).to(device)
+        self.actor_local  = Actor(state_size, action_size, random_seed, fc1_units=layer1_units, fc2_units=layer2_units).to(device)
+        self.actor_target = Actor(state_size, action_size, random_seed, fc1_units=layer1_units, fc2_units=layer2_units).to(device)
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
 
         # Critic Network (w/ Target Network)
-        self.critic_local = Critic(state_size, action_size, random_seed).to(device)
-        self.critic_target = Critic(state_size, action_size, random_seed).to(device)
+        self.critic_local  = Critic(state_size, action_size, random_seed, fcs1_units=layer1_units, fc2_units=layer2_units).to(device)
+        self.critic_target = Critic(state_size, action_size, random_seed, fcs1_units=layer1_units, fc2_units=layer2_units).to(device)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
 
         # Noise process
         self.noise = OUNoise(action_size, random_seed)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, self.batch_size, random_seed)
     
     def step(self, state, action, reward, next_state, done):
         """Save experience in replay memory, and use random sample from buffer to learn."""
@@ -57,7 +61,7 @@ class DdpgAgent():
         self.memory.add(state, action, reward, next_state, done)
 
         # Learn, if enough samples are available in memory
-        if len(self.memory) > BATCH_SIZE:
+        if len(self.memory) > self.batch_size:
             experiences = self.memory.sample()
             self.learn(experiences, GAMMA)
 
@@ -70,8 +74,9 @@ class DdpgAgent():
         self.actor_local.train()
         if add_noise:
             action += self.noise.sample()
-        action = (action + 1.0) / 2.0
-        return np.clip(action, 0, 1)
+        # use next line if we want to constrain output to [0, 1]. Input coming from actor_local is already in [-1, 1].
+        #action = (action + 1.0) / 2.0
+        return np.clip(action, -1, 1)
 
 
     def reset(self):
@@ -92,29 +97,36 @@ class DdpgAgent():
         states, actions, rewards, next_states, dones = experiences
 
         # ---------------------------- update critic ---------------------------- #
+
         # Get predicted next-state actions and Q values from target models
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
+
         # Compute Q targets for current states (y_i)
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        Q_targets = rewards + gamma*Q_targets_next*(1 - dones)
+
         # Compute critic loss
         Q_expected = self.critic_local(states, actions)
         critic_loss = F.mse_loss(Q_expected, Q_targets)
+
         # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
 
         # ---------------------------- update actor ---------------------------- #
+
         # Compute actor loss
         actions_pred = self.actor_local(states)
         actor_loss = -self.critic_local(states, actions_pred).mean()
+
         # Minimize the loss
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         self.actor_optimizer.step()
 
         # ----------------------- update target networks ----------------------- #
+
         self.soft_update(self.critic_local, self.critic_target, TAU)
         self.soft_update(self.actor_local, self.actor_target, TAU)                     
 
